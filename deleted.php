90d477ad3c53bbf03f601240961f9a958d934fe8
Zafhkiel <162438131+zafhkiel21@users.noreply.github.com>
2024-05-10 17:38:33 +0800

    Update deleted.php

--- deleted.php
+++ deleted.php
@@ -102,7 +102,7 @@ $conn->close();
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
-    <form method="post" class="Delete">
+    <form method="post">
     <div  class="submit-button"><input type="Submit"  name="delete"></div>
     </form>      
   
@@ -128,6 +128,7 @@ $conn->close();
                       <th scope="col">Contact Number</th>
                       <th scope="col">Email</th>
                       <th scope="col">Type of Appointment</th>
+
               </thead>
               <tbody>
                 
